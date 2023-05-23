75233febfbcdb9b305b20a253d150dfbcc0e7d75
Santiago E <santy.e@gmail.com>
2023-05-22 22:19:40 -0500

    Fix Betty

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -20,20 +20,20 @@ void swap(int *a, int *b)
  */
 void selection_sort(int *array, size_t size)
 {
-    size_t i, j;
+	size_t i, j;
 
-    for (i = 0; i < size - 1; i++)
-    {
-        size_t min_idx = i;
-        for (j = i + 1; j < size; j++)
-        {
-            if (array[j] < array[min_idx])
-                min_idx = j;
-        }
-        if (min_idx != i)
-        {
-            swap(&array[i], &array[min_idx]);
-            print_array(array, size);
-        }
-    }
+	for (i = 0; i < size - 1; i++)
+	{
+		size_t min_idx = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min_idx])
+				min_idx = j;
+		}
+		if (min_idx != i)
+		{
+			swap(&array[i], &array[min_idx]);
+			print_array(array, size);
+		}
+	}
 }
