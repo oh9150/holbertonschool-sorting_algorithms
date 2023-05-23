bcd0a94633b0b35dd6729dd3eb7c4bd9967094bc
Santiago E <santy.e@gmail.com>
2023-05-22 22:16:25 -0500

    Fix loop for

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -20,23 +20,20 @@ void swap(int *a, int *b)
  */
 void selection_sort(int *array, size_t size)
 {
-	if (array == NULL || size < 2)
-		return;
+    size_t i, j;
 
-	for (size_t i = 0; i < size - 1; i++)
-	{
-		size_t min_idx = i;
-
-		for (size_t j = i + 1; j < size; j++)
-		{
-			if (array[j] < array[min_idx])
-			min_idx = j;
-		}
-
-		if (min_idx != i)
-		{
-			swap(&array[i], &array[min_idx]);
-			print_array(array, size);
-		}
-	}
+    for (i = 0; i < size - 1; i++)
+    {
+        size_t min_idx = i;
+        for (j = i + 1; j < size; j++)
+        {
+            if (array[j] < array[min_idx])
+                min_idx = j;
+        }
+        if (min_idx != i)
+        {
+            swap(&array[i], &array[min_idx]);
+            print_array(array, size);
+        }
+    }
 }
