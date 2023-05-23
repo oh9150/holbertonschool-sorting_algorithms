3710e9ed4bf3221a8c1c73acc7e84102e61574fb
root <6351@holbertonstudents.com>
2023-05-22 19:27:23 -0700

    NULL case and betty fixes

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,6 +9,10 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
 
+	if (!array || size == 0)
+		return;
+
+
 	for (i = 0; i < size - 1; i++)
 	{
 		for (j = 0; j < size - i - 1; j++)
@@ -23,6 +27,11 @@ void bubble_sort(int *array, size_t size)
 	}	
 }
 
+/**
+ * swap - swaps @a's and @b's values
+ * @a: int #1
+ * @b: int #2
+ */
 void swap(int *a, int *b)
 {
 	int tmp;
--- sort.h
+++ sort.h
@@ -28,42 +28,66 @@ void print_list(const listint_t *list);
 void swap(int *a, int *b);
 
 /*Mandatory*/
-/*0. function that sorts an array of integers in ascending order using the Bubble sort algorithm*/
+/*0. function that sorts an array of integers in ascending order using the
+ * Bubble sort algorithm
+ */
 void bubble_sort(int *array, size_t size);
 
-/*1. function that sorts a doubly linked list of integers in ascending order using the Insertion sort algorithm*/
+/*1. function that sorts a doubly linked list of integers in ascending order
+ * using the Insertion sort algorithm
+ */
 void insertion_sort_list(listint_t **list);
 
-/*2. function that sorts an array of integers in ascending order using the Selection sort algorithm*/
+/*2. function that sorts an array of integers in ascending order using the
+ * Selection sort algorithm
+ */
 void selection_sort(int *array, size_t size);
 
-/*3. function that sorts an array of integers in ascending order using the Quick sort algorithm*/
+/*3. function that sorts an array of integers in ascending order using the
+ * Quick sort algorithm
+ */
 void quick_sort(int *array, size_t size);
 
 
 /*Advanced*/
-/*4. function that sorts an array of integers in ascending order using the Shell sort algorithm, using the Knuth sequence*/
+/*4. function that sorts an array of integers in ascending order using the
+ * Shell sort algorithm, using the Knuth sequence
+ */
 void shell_sort(int *array, size_t size);
 
-/*5. function that sorts a doubly linked list of integers in ascending order using the Cocktail shaker sort algorithm*/
+/*5. function that sorts a doubly linked list of integers in ascending order
+ * using the Cocktail shaker sort algorithm
+ */
 void cocktail_sort_list(listint_t **list);
 
-/*6. function that sorts an array of integers in ascending order using the Counting sort algorithm*/
+/*6. function that sorts an array of integers in ascending order using the
+ * Counting sort algorithm
+ */
 void counting_sort(int *array, size_t size);
 
-/*7. function that sorts an array of integers in ascending order using the Merge sort algorithm*/
+/*7. function that sorts an array of integers in ascending order using the
+ * Merge sort algorithm
+ */
 void merge_sort(int *array, size_t size);
 
-/*8. function that sorts an array of integers in ascending order using the Heap sort algorithm*/
+/*8. function that sorts an array of integers in ascending order using the
+ * Heap sort algorithm
+ */
 void heap_sort(int *array, size_t size);
 
-/*9. function that sorts an array of integers in ascending order using the Radix sort algorithm*/
+/*9. function that sorts an array of integers in ascending order using the
+ * Radix sort algorithm
+ */
 void radix_sort(int *array, size_t size);
 
-/*10. function that sorts an array of integers in ascending order using the Bitonic sort algorithm*/
+/*10. function that sorts an array of integers in ascending order using the
+ * Bitonic sort algorithm
+ */
 void bitonic_sort(int *array, size_t size);
 
-/*11. function that sorts an array of integers in ascending order using the Quick sort algorithm*/
+/*11. function that sorts an array of integers in ascending order using the
+ * Quick sort algorithm
+ */
 void quick_sort_hoare(int *array, size_t size);
 
 #endif
