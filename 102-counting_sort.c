5782bbb1ae7a27e18285dc867d7db1e45e3195ad
Santiago E <santy.e@gmail.com>
2023-05-26 17:50:38 -0500

    Fix

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -8,42 +8,44 @@
  */
 void counting_sort(int *array, size_t size)
 {
-	int max_element, i;
-	int *count_array, *sorted_array;
+	int n, i;
+	int *buff, *a;
 
 	if (size < 2)
 		return;
 
-	max_element = array[0];
+	for (n = i = 0; i < (int)size; i++)
+		if (array[i] > n)
+			n = array[i];
 
-	for (i = 1; i < (int)size; i++)
+	buff = malloc(sizeof(int) * (n + 1));
+	if (!buff)
+		return;
+
+	for (i = 0; i <= n; i++)
+		buff[i] = 0;
+	for (i = 0; i < (int)size; i++)
+		buff[array[i]] += 1;
+	for (i = 1; i <= n; i++)
+		buff[i] += buff[i - 1];
+
+	print_array(buff, (n + 1));
+	a = malloc(sizeof(int) * (size + 1));
+
+	if (!a)
 	{
-		if (array[i] > max_element)
-			max_element = array[i];
+		free(buff);
+		return;
+	}
+	for (i = 0; i < (int)size; i++)
+	{
+		a[buff[array[i]] - 1] = array[i];
+		buff[array[i]] -= 1;
 	}
 
-	count_array = malloc(sizeof(int) * (max_element + 1));
-	if (!count_array)
-		return;
-	for (i = 0; i <= max_element; i++)
-		count_array[i] = 0;
 	for (i = 0; i < (int)size; i++)
-		count_array[array[i]]++;
-	for (i = 1; i <= max_element; i++)
-		count_array[i] += count_array[i - 1];
-	sorted_array = malloc(sizeof(int) * size);
-	if (!sorted_array)
-	{
-		free(count_array);
-		return;
-	}
-	for (i = 0; i < (int)size; i++)
-	{
-		sorted_array[count_array[array[i]] - 1] = array[i];
-		count_array[array[i]]--;
-	}
-	for (i = 0; i < (int)size; i++)
-		array[i] = sorted_array[i];
-	free(count_array);
-	free(sorted_array);
+		array[i] = a[i];
+
+	free(buff);
+	free(a);
 }
