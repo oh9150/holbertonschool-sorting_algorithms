f45568e04c60a7bdb86b4601ae4fe1fd2b37b130
root <6351@holbertonstudents.com>
2023-05-26 16:20:13 -0700

    shell sort

---
+++ 100-shell_sort.c
@@ -0,0 +1,44 @@
+#include "sort.h"
+
+/**
+ * shell_sort - sorts an array or integers in ascending order using the shell
+ * sort algorithm, using the knuth sequence
+ * @array: the array to sort
+ * @size: the size of @array
+ */
+void shell_sort(int *array, size_t size)
+{
+	int n = 0;
+	size_t i;
+
+	if (!array)
+		return;
+
+	for (; n < size; n = n * 3 + 1)
+	{
+		printf("n = %d\n", n);
+
+		for (i = n; i < size; i++)
+		{
+			if (array[i - n] > array[i])
+			{
+				printf("swapping %d and %d\n", array[i - n], array[i]);
+				swap(&array[i - n], &array[i]);
+				print_array(array, size);
+			}
+		}
+	}
+}
+
+/**
+ * swap - swaps two integers in an array
+ * @a: int #1
+ * @b: int #2
+ */
+void swap(int *a, int *b)
+{
+	int tmp = *a;
+ 
+	*a = *b;
+	*b = tmp;
+}
