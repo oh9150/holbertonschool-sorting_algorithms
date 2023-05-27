98a70904063aa83ef5a1ff59fba41d6dd8f0f221
Santiago E <santy.e@gmail.com>
2023-05-26 20:38:11 -0500

    Fix

--- 105-radix_sort.c
+++ 105-radix_sort.c
@@ -8,72 +8,28 @@
  */
 void radix_sort(int *array, size_t size)
 {
-	int max, exp;
-	int *count, *output;
-	int last_step = 0;  /* Variable de control para el último paso */
+	int flag = 1, n = 10;
+	size_t i, f;
 
-	max = get_max(array, size);
-	count = malloc(sizeof(int) * 10);
-	output = malloc(sizeof(int) * size);
-	if (!count || !output)
-	{
-		free(count);
-		free(output);
+	if (!array || size == 1)
 		return;
-	}
-
-	for (exp = 1; max / exp > 0; exp *= 10)
+	while (flag)
 	{
-		int i;
-
-		for (i = 0; i < 10; i++)
-			count[i] = 0;
-
-		for (i = 0; i < (int)size; i++)
-			count[(array[i] / exp) % 10]++;
-
-		for (i = 1; i < 10; i++)
-			count[i] += count[i - 1];
-
-		for (i = size - 1; i >= 0; i--)
+		flag = 0;
+		for (i = 1, f = 1; i <  size; i++, f++)
 		{
-			output[count[(array[i] / exp) % 10] - 1] = array[i];
-			count[(array[i] / exp) % 10]--;
+			if ((array[i - 1] % (n * 10)) / ((n * 10) / 10) > 0)
+				flag = 1;
+			if (((array[i - 1] % n) / (n / 10)) > ((array[i] % n) / (n / 10)))
+			{
+				array[i - 1] = array[i - 1] + array[i];
+				array[i] = array[i - 1] - array[i];
+				array[i - 1] = array[i - 1] - array[i];
+				if ((i - 1) > 0)
+					i = i - 2;
+			}
 		}
-
-		for (i = 0; i < (int)size; i++)
-			array[i] = output[i];
-
-		if (exp != 1)  /* Si no es el último paso, imprimir el array */
-			print_array(array, size);
-
-		if (exp * 10 > max)  /* Último paso, actualizar variable de control */
-			last_step = 1;
-	}
-
-	free(count);
-	free(output);
-
-	if (!last_step)  /* Si no es el último paso, imprimir el array */
 		print_array(array, size);
-}
-
-/**
- * get_max - Returns the maximum element in an array of integers
- * @array: The array of integers
- * @size: The size of the array
- * Return: The maximum element
- */
-int get_max(int *array, size_t size)
-{
-	int max = array[0];
-	size_t i;
-
-	for (i = 1; i < size; i++)
-	{
-		if (array[i] > max)
-			max = array[i];
+		n = n * 10;
 	}
-
-	return (max);
 }
